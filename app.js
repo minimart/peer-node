233141db15a4f66bce68e22868842c2cc53e05b9
Megan Martinson <hello@meg.codes>
2016-07-10 00:52:31 -0500

    forgot to comment- oops

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var lowestNumber = 0;
 var totalSum = 0;
 var averageNumber = 0;
 
+// this function will take in the text file and make sure that there are no errors, and puts the numbers
+// into a readable array
 function getNumbers(callback){
 fs.readFile('numbers.txt', 'utf-8', function(err, fileContents){
   if(err) {
@@ -13,6 +15,8 @@ fs.readFile('numbers.txt', 'utf-8', function(err, fileContents){
     callback(numbers);
   });
 }
+// this function does all the heavy lifting- takes the array of numbers and find the highest, lowestNumber
+// and the average and console logs them out!
 function done(numbers) {
   for(var i = 0; i<numbers.length; i++){
     numbers[i]=parseInt(numbers[i]);
